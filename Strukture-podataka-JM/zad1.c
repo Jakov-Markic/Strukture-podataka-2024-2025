0a69e8c2ec13d7c29938f05525d14d8d71f5cd28
lzeljak <lovrezeljak123@gmail.com>
2024-10-15 19:06:18 +0200

    Prebacivanje na visual studio

--- Strukture-podataka-JM/zad1.c
+++ Strukture-podataka-JM/zad1.c
@@ -1,5 +1,5 @@
 /*Napisati program koji prvo procita koliko redaka ima datoteka, tj. koliko ima sudenata zapisanih u datoteci.
-Nakon toga potrebno je dinamicki alocirati prostor za niz struktura studenata (ime, prezime, bodovi) i 
+Nakon toga potrebno je dinamicki alocirati prostor za niz struktura studenata (ime, prezime, bodovi) i
 ucitati iz datoteke sve zapise. Na ekran ispisati ime, prezime, apsolutni i relativni broj bodova.
 Napomena: Svaki redak datoteke sadrzi ime i prezime studenata, te broj bodova na kolokviju.
 relativan_br_bodova = br_bodova/max_br_bodova*100  */
@@ -14,21 +14,21 @@ relativan_br_bodova = br_bodova/max_br_bodova*100  */
 #define MAX_BROJ_BODOVA 50
 typedef struct
 {
-	char *ime;
-	char *prezime;
+	char* ime;
+	char* prezime;
 	int bodovi;
 
 
 }Student;
 
 int count_rows(char* filename);
-void get_student_data(Student * s, char * filename);
-void print_student(Student * s, char * filename, int rows);
+void get_student_data(Student* s, char* filename);
+void print_student(Student* s, char* filename, int rows);
 
 int main() {
 
 	int row_count = count_rows(STUDENTI_FILE);
-	Student * s = (Student *) malloc (row_count * sizeof(Student)); 
+	Student* s = (Student*)malloc(row_count * sizeof(Student));
 	get_student_data(s, &STUDENTI_FILE);
 	printf("Ispis %d studenata i njegovih podataka:\n\n", row_count);
 	print_student(s, STUDENTI_FILE, row_count);
@@ -36,16 +36,16 @@ int main() {
 	return 0;
 }
 
-void print_student(Student * s, char * filename, int rows){
+void print_student(Student* s, char* filename, int rows) {
 	int i = 0;
-	for(i; i < rows; i++){
+	for (i; i < rows; i++) {
 		printf("%s %s \n", s[i].ime, s[i].prezime);
 		printf("\tApsolutan broj bodova: %d\n", s[i].bodovi);
-		printf("\tRelativan broj bodova %f %%\n\n", ((float) s[i].bodovi / MAX_BROJ_BODOVA) * 100);
+		printf("\tRelativan broj bodova %f %%\n\n", ((float)s[i].bodovi / MAX_BROJ_BODOVA) * 100);
 	}
 }
 
-void get_student_data(Student * s, char * filename){
+void get_student_data(Student* s, char* filename) {
 	FILE* fp = NULL;
 	char buffer[BUFFER_SIZE];
 	int i = 0;
@@ -62,14 +62,14 @@ void get_student_data(Student * s, char * filename){
 		if (buffer[0] != ' ' && buffer[0] != '\n') //provjera ako postoji prazan redak
 		{
 			//Student ima jedno ime, a ostalo je prezime
-			s[i].ime = (char *) malloc (50 * sizeof(char));
-			s[i].prezime = (char *) malloc (100 * sizeof(char));
+			s[i].ime = (char*)malloc(50 * sizeof(char));
+			s[i].prezime = (char*)malloc(100 * sizeof(char));
 			sscanf(buffer, "%s %[^0-9] %d", s[i].ime, s[i].prezime, &s[i].bodovi);
 			++i;
 		}
-		
+
 	}
-	
+
 
 	fclose(fp);
 }
@@ -90,7 +90,7 @@ int count_rows(char* filename) {
 		fgets(buffer, BUFFER_SIZE, fp);
 		//sscanf()
 		if (buffer[0] != ' ' && buffer[0] != '\n') // provjera ako ima prazan red
-		++count;
+			++count;
 	}
 
 	fclose(fp);
