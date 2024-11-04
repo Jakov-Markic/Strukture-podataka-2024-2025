c8d549fa1916a466c2ea18d064afc1d847250a9a
Jakov-Markic <jakov.markic358@gmail.com>
2024-11-04 20:52:18 +0100

    skoro gotova vj3

--- Zad1/Zad3/zad3.c
+++ Zad1/Zad3/zad3.c
@@ -20,11 +20,16 @@ E. cita listu iz datoteke.
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #define ERROR_CREATING_PERSON 10
 #define ERROR_FIND_PERSON 15
+#define ERROR_OPENING_FILE -1
+
 #define NAME_LENGHT 30
 #define SURNAME_LENGHT 50
+
+#define BUFFER_SIZE 1024
 
 struct _Osoba;
 typedef struct _Osoba* Position;
@@ -46,9 +51,11 @@ void deletePerson_BySurname(char* prezime, Position Person);
 
 int newPerson_BeforeP(char * findIme, char * ime, char * prezime, int godina, Position Person);
 int newPerson_AfterP(char * findIme, char* ime, char* prezime, int godina, Position Person);
-void sortPerson(Position Person);
-void readFile_Person(Position Person);
-void writeFile_Person(Position Person);
+void sortPerson_bySurname(Position Person);
+
+Position readFile_Person(char * filename);
+int checkifLineEmpty(char* line);
+int writeFile_Person(Position Person);
 
 Position findPreviousPerson_bySurname(char * prezime, Position Person);
 
@@ -77,6 +84,7 @@ int main() {
     }
 
     printPerson(Head.next);
+    printf("\n------------------------\n");
 
     //checkOsoba ako zelimo nesto raditi s tim podacima
     checkOsoba = findPerson_bySurname("Antic", Head.next);
@@ -87,11 +95,25 @@ int main() {
     deletePerson_BySurname("Antonovic", Head.next);
 
     printPerson(Head.next);
+    printf("\n------------------------\n");
 
     newPerson_AfterP("Antic", "Antun", "Antunovic", 1995, &Head);
     printPerson(Head.next);
+    printf("\n------------------------\n");
 
     newPerson_BeforeP("Antic", "Stipe", "Stipic", 1988, &Head);
+    newPerson_BeforeP("Antic", "Luka", "Lukic", 1977, &Head);
+    printPerson(Head.next);
+
+    printf("\n------------------------\n");
+
+    printf("\nSortiranje liste\n");
+    sortPerson_bySurname(&Head);
+    printPerson(Head.next);
+
+    writeFile_Person(Head.next);
+    Head.next = readFile_Person("studenti.txt");
+    printf("\n------------------------\n");
     printPerson(Head.next);
 
 
@@ -120,6 +142,7 @@ Position create_Person(char* ime, char* prezime, int godina) {
     strcpy(q->prezime, prezime);
 
     q->godina_rodjenja = godina;
+    q->next = NULL;
 
     return q;
 }
@@ -167,15 +190,6 @@ int newPerson_BeforeP(char* findIme, char* ime, char* prezime, int godina, Posit
     Person->next = q;
 }
 
-Position findPreviousPerson_bySurname(char* prezime, Position Person) {
-    while (Person != NULL && strcmp(Person->next->prezime, prezime) != 0)
-        Person = Person->next;
-    if (Person == NULL)
-        printf("\nOsoba s %s prezimenom ne postoji  :( \n", prezime);
-
-    return Person;
-}
-
 int newPerson_AfterP(char* findIme, char* ime, char* prezime, int godina, Position Person) {
     Person = findPerson_bySurname(findIme, Person->next);
     if (Person == NULL) {
@@ -188,6 +202,15 @@ int newPerson_AfterP(char* findIme, char* ime, char* prezime, int godina, Positi
     q->next = Person->next;
     Person->next = q;
 
+}
+
+Position findPreviousPerson_bySurname(char* prezime, Position Person) {
+    while (Person != NULL && strcmp(Person->next->prezime, prezime) != 0)
+        Person = Person->next;
+    if (Person == NULL)
+        printf("\nOsoba s %s prezimenom ne postoji  :( \n", prezime);
+
+    return Person;
 }
 
 Position findPerson_bySurname(char* prezime, Position Person) {
@@ -230,6 +253,108 @@ void printPerson(Position Person) {
         Person = Person->next;
     }
 }
-void sortPerson(Position Person) {}
-void readFile_Person(Position Person) {}
-void writeFile_Person(Position Person) {}
+void sortPerson_bySurname(Position Person) {
+
+    //nije lipo a ni optimizirano ali radi
+
+    Position temp1 = Person;
+    Position temp2 = Person->next;
+    Position temp3 = Person->next->next;
+    Position end = NULL;
+    Position temp = NULL;
+    while (end != Person->next->next) {
+        temp1 = Person;
+        temp2 = Person->next;
+        temp3 = Person->next->next;
+        temp = NULL;
+        while (temp2->next != end) {
+            if (strcmp(temp2->prezime, temp3->prezime) > 0) {
+                temp = temp2;
+                temp2->next = temp3->next;
+                temp3->next = temp;
+                temp1->next = temp3;
+
+                temp1 = temp1->next;
+                temp3 = temp2->next;
+            }
+            else {
+                temp1 = temp1->next;
+                temp2 = temp2->next;
+                temp3 = temp3->next;
+            }
+
+
+        }
+        end = temp2;
+    }
+
+}
+Position readFile_Person(char * filename) {
+    FILE* fp = NULL;
+
+    Position q = NULL;
+    Position first = NULL;
+    Position temp = NULL;
+
+    char buffer[BUFFER_SIZE];
+    char ime[NAME_LENGHT];
+    char prezime[SURNAME_LENGHT];
+    int godina;
+
+    int check = 0;
+    int isFirstCreated = 1;
+
+    fp = fopen(filename, "r");
+    if (fp == NULL) {
+        printf("ERROR opening a file\n");
+        return NULL;
+    }
+
+    while (!feof(fp)) {
+        fgets(buffer, BUFFER_SIZE, fp);
+        if (!checkifLineEmpty(buffer)) {
+            check = sscanf(buffer, "%s %[^0-9] %d", ime, prezime, &godina);
+
+            if (check != 3) {
+                printf("ERROR reading file");
+                return NULL;
+            }
+            q = create_Person(ime, prezime, godina);
+            if (isFirstCreated) {
+                first = q;
+                temp = first;
+                isFirstCreated = 0;
+            }
+            temp->next = q;
+            temp = temp->next;
+        }
+    }
+    return first;
+}
+int checkifLineEmpty(char * line)
+{
+    char* ch = line;
+    int is_blank = -1;
+
+    for (ch; *ch != '\0' && is_blank != 0; ch++)
+    {
+        if (!isspace(*ch))
+            is_blank = 0;   
+    }
+
+    return is_blank;
+}
+
+int writeFile_Person(Position Person) {
+    FILE* fp = NULL;
+    
+    fp = fopen("Osobe.txt", "w");
+    if (fp == NULL) {
+        printf("ERROR opening a file\n");
+        return ERROR_OPENING_FILE;
+    }
+    while (Person != NULL) {
+        fprintf_s(fp, "%-10s\t%-20s\t%d\n", Person->ime, Person->prezime, Person->godina_rodjenja);
+        Person = Person->next;
+    }
+}
